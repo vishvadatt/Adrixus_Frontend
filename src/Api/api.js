b281f72eecb2249531e519b19b40f2710e0c61cd
vishwadatt <vishwadattmaharaulji@gmail.com>
2023-03-15 14:29:47 +0530

    upload live api

--- src/Api/api.js
+++ src/Api/api.js
@@ -6,13 +6,13 @@ export const getUserApi = async (data) => {
     const {search,pageNo,age} = data;
     // return await axios.get(`${baseUrl}user/get-users?search=${search}&pageNo=${pageNo}`);
     // return await axios.get(`${baseUrl}get-users?${search}&pageNo=${pageNo}&age=${age}`);
-    return await axios.get(`http://localhost:8001/api/get-users?search=${search}&pageNo=${pageNo}&age=${age}`)
+    return await axios.get(`https://adrixus-backend.onrender.com/api/get-users?search=${search}&pageNo=${pageNo}&age=${age}`)
 }
 
 export const userSignUpApi = async (data) => {
-    return await axios.post('http://localhost:8001/api/auth/signup',data);
+    return await axios.post('https://adrixus-backend.onrender.com/api/auth/signup',data);
 }
 
 export const userSignInApi = async (data) => {
-    return await axios.post("http://localhost:8001/api/auth/login",data);
+    return await axios.post("https://adrixus-backend.onrender.com/api/auth/login",data);
 }
